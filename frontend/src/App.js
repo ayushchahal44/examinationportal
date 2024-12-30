cb877035484a26e472499bd89133ff09cb3d37d3
Disha071 <disha.gupta_cs22@gla.ac.in>
2024-12-30 22:33:35 +0530

    examination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-// App.js
+
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
